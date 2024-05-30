f9e743fe6fe90085293fdc414ff56bfedb2a15ab
kaytervn <kienductrong@gmail.com>
2024-05-30 08:07:01 +0700

    xong part10

---
+++ restful-web-services/src/main/java/kaytervn/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,23 @@
+package kaytervn.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world bean");
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello world, " + name);
+	}
+}
