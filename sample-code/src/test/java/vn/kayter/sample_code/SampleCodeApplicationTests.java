4e86fb16c252121dd5c9783c0e24d6dc1094ce38
kaytervn <kienductrong@gmail.com>
2024-06-01 10:39:23 +0700

    init sample code

---
+++ sample-code/src/test/java/vn/kayter/sample_code/SampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.kayter.sample_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
