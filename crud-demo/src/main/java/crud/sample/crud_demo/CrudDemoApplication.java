c0868639aea29952839d7d814f82a470650da22b
kaytervn <kienductrong@gmail.com>
2024-06-04 20:40:13 +0700

    init crud

---
+++ crud-demo/src/main/java/crud/sample/crud_demo/CrudDemoApplication.java
@@ -0,0 +1,13 @@
+package crud.sample.crud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+}
---
+++ crud-demo/src/test/java/crud/sample/crud_demo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.sample.crud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
