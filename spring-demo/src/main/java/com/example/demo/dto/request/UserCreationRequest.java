622152e46b7d2c14098add3fec56f34d0f15c147
kaytervn <kienductrong@gmail.com>
2024-06-23 11:31:24 +0700

    remove unused annotate

--- spring-demo/src/main/java/com/example/demo/dto/request/UserCreationRequest.java
+++ spring-demo/src/main/java/com/example/demo/dto/request/UserCreationRequest.java
@@ -5,16 +5,11 @@ import com.example.demo.enums.Gender;
 import com.example.demo.validator.DobConstraint;
 import com.example.demo.validator.EnumConstraint;
 import com.example.demo.validator.UniqueConstraint;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
--- spring-demo/src/main/java/com/example/demo/entity/Post.java
+++ spring-demo/src/main/java/com/example/demo/entity/Post.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDate;
-
 @Getter
 @Setter
 @Builder
